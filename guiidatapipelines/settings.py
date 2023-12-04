967ac1368547c95989b8300aa332ac1e37f0b21d
siddu1998 <samaram@ucsc.edu>
2023-12-03 17:43:22 -0800

    test

--- guiidatapipelines/settings.py
+++ guiidatapipelines/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-import django_heroku
+import django_on_heroku as django_heroku 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
